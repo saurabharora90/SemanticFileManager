ed6ef70efd5aa32d3ae648b7d6650f481f19eabf
Saurabh Arora <saurabharora90@gmail.com>
2026-01-04 17:32:53 -0800

    Add lint baseline

--- app/src/main/kotlin/dev/bongballe/semanticfilemanager/MainActivity.kt
+++ app/src/main/kotlin/dev/bongballe/semanticfilemanager/MainActivity.kt
@@ -3,7 +3,6 @@ package dev.bongballe.semanticfilemanager
 import android.Manifest
 import android.app.Activity
 import android.content.Intent
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
